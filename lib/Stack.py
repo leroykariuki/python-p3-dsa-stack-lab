2ad11fa76bd86591c214f32282c9b44c6ee8faeb
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-13 14:43:31 +0300

    done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,40 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
-
-    def isEmpty(self):
-        pass
+    def __init__(self, limit=None):
+        if isinstance(limit, list):
+            self.items = list(limit)
+            self.limit = None  # No limit for list initialization
+        else:
+            self.items = []
+            self.limit = limit
 
     def push(self, item):
-        pass
+        if self.limit is None or len(self.items) < int(self.limit):
+            self.items.append(item)
+        else:
+            raise Exception("Stack is full")
 
     def pop(self):
-        pass
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise Exception("Stack is empty")
 
     def peek(self):
-        pass
-    
+        if not self.is_empty():
+            return self.items[-1]
+
     def size(self):
-        pass
+        return len(self.items)
 
-    def full(self):
-        pass
+    def is_empty(self):
+        return len(self.items) == 0
 
-    def search(self, target):
-        pass
+    def is_full(self):
+        return self.limit is not None and len(self.items) == self.limit
+
+    def search(self, value):
+        try:
+            index = self.items.index(value)
+            return len(self.items) - index
+        except ValueError:
+            return -1
--- lib/testing/stack_test.py
+++ lib/testing/stack_test.py
@@ -1,72 +1,73 @@
-from Stack import Stack
+from lib.Stack import Stack
+
 
 class TestStack:
-    '''Class Stack in Stack.py'''
-
     def test_init(self):
         '''Initialize Stack with list'''
-        stk = Stack([1,2,3,4,5])
-        expected = [1,2,3,4,5]
+        stk = Stack([1, 2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
         for index in range(len(expected)):
             assert(expected[index] == stk.items[index])
 
     def test_push(self):
         '''Push 0 to stack'''
-        stk = Stack([1,2,3,4,5])
+        stk = Stack([1, 2, 3, 4, 5])
         stk.push(0)
-        expected = [1,2,3,4,5,0]
-        for index in range(0,len(expected)):
+        expected = [1, 2, 3, 4, 5, 0]
+        for index in range(0, len(expected)):
             assert(expected[index] == stk.items[index])
 
     def test_pop(self):
         '''Pop 1 off the stack'''
-        stk = Stack([1,2,3,4,5])
+        stk = Stack([1, 2, 3, 4, 5])
         stk.pop()
-        expected = [1,2,3,4]
+        expected = [1, 2, 3, 4]
         for index in range(len(expected)):
             assert(expected[index] == stk.items[index])
 
     def test_size(self):
         '''Test Stack size() method'''
-        stk = Stack([1,2,3,4,5])
-        expected = [1,2,3,4,5]
+        stk = Stack([1, 2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
         assert(stk.size() == len(expected))
 
     def test_empty(self):
-        '''Test Stack empty() method'''
+        '''Test Stack is_empty() method'''
         stk = Stack()
-        assert(stk.isEmpty())
+        assert(stk.is_empty())
         assert(stk.size() == 0)
-        assert(stk.pop() == None)
+        try:
+            stk.pop()
+            assert False, "Expected Exception"
+        except Exception as e:
+            assert str(e) == "Stack is empty"
         stk.push(1)
-        assert(not stk.isEmpty())
+        assert(not stk.is_empty())
         assert(stk.size() == 1)
         assert(stk.pop() == 1)
-
 
     def test_full(self):
-        '''Test Stack full() method'''
-        stk = Stack([1], 1)
-
-        assert(stk.full())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
+        '''Test Stack is_full() method'''
+        stk = Stack(limit=1)
+        assert(not stk.is_full())
         stk.push(1)
-        stk.push(2)
-        assert(stk.full())
+        assert(stk.is_full())
         assert(stk.size() == 1)
         assert(stk.pop() == 1)
 
-    def test_search(self):
-        '''Test Stack search() method. How far is the element in the stack? '''
-        stk = Stack([5,6,7,8,9,10])
+        stk.push(1)
+        try:
+            stk.push(2)
+            assert False, "Expected Exception"
+        except Exception as e:
+            assert str(e) == "Stack is full"
 
-        assert(stk.search(5) == 5)
-        assert(stk.search(6) == 4)
-        assert(stk.search(7) == 3)
-        assert(stk.search(8) == 2)
-        assert(stk.search(9) == 1)
-        assert(stk.search(10) == 0)
+    def search(self, value):
+        try:
+         index = self.items.index(value)
+         return len(self.items) - index
+        except ValueError:
+         return -1
 
         # Case with target not in Stack
         assert(stk.search(15) == -1)
